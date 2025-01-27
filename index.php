edb72eb948b622166c87fb377aadb6284127c1fe
bwilm1 <brodenw55@gmail.com>
2025-01-27 15:10:42 -0700

    fixed some stuff

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
  * Version: 1.0.0
  * License: GNU General Public License v2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: custom-pirate-demo
+ * Text Domain: pirates-rugby-theme
  *
- * @package custom-pirate-demo
+ * @package pirates-rugby-theme
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
