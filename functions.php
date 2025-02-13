33ad3cc69753bdc9fab93765f6913c3f46c62191
Dwayne Aquan <dwayneaquan@gmail.com>
2025-02-13 15:19:47 -0700

    set up sass

--- functions.php
+++ functions.php
@@ -6,6 +6,17 @@ function my_custom_block_theme_assets() {
 }
 add_action('wp_enqueue_scripts', 'my_custom_block_theme_assets');
 
+function custom_scss_style() {
+  wp_enqueue_style(
+    'custom-scss-style',
+    get_stylesheet_directory_uri() . "/custom-style.css",
+    array(),
+    null,
+    'all'
+  );
+}
+add_action('wp_enqueue_scripts', 'custom_scss_style');
+
 // Add support for Full Site Editing (FSE)
 add_theme_support('block-templates');
 add_theme_support('editor-styles');
