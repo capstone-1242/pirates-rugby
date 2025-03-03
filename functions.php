0e7e59b0ad767c04631e4a389d0c1b8dc701000c
bwilm1 <brodenw55@gmail.com>
2025-03-03 13:33:37 -0700

    Add custom SCSS styles and enqueue stylesheets for the theme

--- functions.php
+++ functions.php
@@ -6,6 +6,27 @@ function my_custom_block_theme_assets() {
 }
 add_action('wp_enqueue_scripts', 'my_custom_block_theme_assets');
 
+<<<<<<< Updated upstream
+=======
+function custom_scss_style() {
+  wp_enqueue_style(
+    'custom-scss-style',
+    get_stylesheet_directory_uri() . "/custom-style.css",
+    array(),
+    null,
+    'all'
+  );
+}
+add_action('wp_enqueue_scripts', 'custom_scss_style');
+
+
+function pirates_rugby_enqueue_styles() {
+  wp_enqueue_style('pirates-rugby-styles', get_template_directory_uri() . '/css/style.css', array(), '1.0.0');
+}
+add_action('wp_enqueue_scripts', 'pirates_rugby_enqueue_styles');
+
+
+>>>>>>> Stashed changes
 // Add support for Full Site Editing (FSE)
 add_theme_support('block-templates');
 add_theme_support('editor-styles');
